a2dc59b5df3ac8efd649e4fa6fa0030adb6ad0f4
Markkex <avila.marco.dev@gmail.com>
2024-10-12 10:55:40 +0100

    changes to metadata, revert back changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,36 +2,24 @@ import { cinzel } from "./fonts/fonts";
 import "./globals.css";
 import Footer from "../components/Footer/Footer";
 import Menu from "../components/Menu/Menu";
-import Head from "next/head";
+
+export const metadata = {
+  title: "Wilson Soares Film",
+  description: "Wilson Soares Film wedding film maker",
+  icons: {
+    icon: "./favicon.ico",
+    shortcut: "./favicon.ico",
+    apple: "./favicon.ico",
+    other: {
+      rel: "logo",
+      url: "./favicon.ico",
+    },
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>Wilson Soares Film</title>
-        <link
-          rel="icon"
-          href="/icon?<generated>"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
-        <link
-          rel="apple-touch-icon"
-          href="/apple-icon?<generated>"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/egjs-flicking/4.x.x/flicking.css"
-          crossorigin="anonymous"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/egjs-flicking/4.x.x/flicking-inline.css"
-          crossorigin="anonymous"
-        />
-      </Head>
       <body className={cinzel.className}>
         <Menu />
         {children}
